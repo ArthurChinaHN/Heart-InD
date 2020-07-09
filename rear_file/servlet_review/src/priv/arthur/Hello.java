1ce7118d9c7ce54e4173c336853eb28f73c6ae9e
arthur <874706273@qq.com>
2020-07-10 07:57:37 +0800

    front and rear

---
+++ rear_file/servlet_review/src/priv/arthur/Hello.java
@@ -0,0 +1,47 @@
+package priv.arthur;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello implements Servlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.print("hellox");
+	}
+
+}
